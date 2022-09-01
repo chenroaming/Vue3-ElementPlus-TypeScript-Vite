c2428f9e725640a43661f3803a921a69452ca05b
chenroaming <chenroaming@foxmail.com>
2022-09-01 14:22:44 +0800

    【feat】:1.需要的安装包引入。2.环境变量文件配置。3.eslint规则配置。

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+  plugins: [
+    'vue',
+    '@typescript-eslint'
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    // 允许非空断言
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    // 允许自定义模块和命名空间
+    '@typescript-eslint/no-namespace': 'off',
+    // 允许对this设置alias
+    '@typescript-eslint/no-this-alias': 'off',
+    // 允许使用require导入语法
+    '@typescript-eslint/no-var-requires': 'off'
+    // 是否允许使用any类型
+    // '@typescript-eslint/no-explicit-any': ['off']
+  }
+}
